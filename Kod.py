8fcdfdcc08cc378441eeec8d9eecd444bd8df540
PottersGame <pottersgame@gmail.com>
2021-05-04 12:12:30 +0200

    Pridal som hashovanie mena pomocou SHA-1 Databaza.txt som zmenil na .ignore
    Edited without permisions (don't do this) by Adam H
    
    Original code by Jakub G

--- Kod.py
+++ Kod.py
@@ -2,13 +2,14 @@ import pyautogui as pg
 import hashlib as hl
 por = pg.confirm("Vyberte si možnosť", "Databaz v1.0.1", buttons=["Prihlásenie", "Registrácia"])
 hashovaneheslo=""
-if por == "Registrácia":
+if por == "Registrácia":    
     name = pg.prompt("Aké je tvoje uživateľské meno?", "Meno", "Sem ho napíš")
+    hashovanename = hl.sha1(name.encode()).hexdigest()
     heslo = pg.password("Vytvorte si svoje heslo", "Heslo", "", "*")
     hashovaneheslo = hl.sha256(heslo.encode()).hexdigest()
-    with open("Databáza.txt","a") as file_:
-        file_.write(str(name))
-        file_.write(';')
+    with open(".ignore","a") as file_:
+        file_.write(str(hashovanename))
+        file_.write('; ')
         file_.write(str(hashovaneheslo))
         file_.write('\n')
 elif por == "Prihlásenie":
@@ -17,7 +18,7 @@ elif por == "Prihlásenie":
         pg.alert("Zadajte prosím meno.")
     if name == None:
         pg.confirm("Chcete ukončiť program?", "Koniec", buttons=("Áno", "Nie"))
-    with open("Databáza.txt","r") as file_:
+    with open(".ignore","r") as file_:
         f=file_.readlines()
         chcecker=False
         for line in f: 
@@ -27,10 +28,11 @@ elif por == "Prihlásenie":
                 docasny_text+=(line[:-1])
             else:
                 docasny_text+=(line)
-            docasny_list=docasny_text.split(";")
+            docasny_list=docasny_text.split("; ")
             k=0
             while k<5:
-                if docasny_list[0] == name:
+                hashovanename = hl.sha1(name.encode()).hexdigest()
+                if docasny_list[0] == hashovanename:
                     chcecker=True
                     heslo = pg.password("Zadajte svoje heslo", "Heslo", "", "*")
                     if heslo == None:
